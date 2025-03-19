e1ff47574610cfaff5daf09015ef3950113cd82d
needsupport <116121446+needsupport@users.noreply.github.com>
2025-03-18 21:15:25 -0700

    Add type declaration file for chrome-remote-interface

--- src/types/chrome-remote-interface.d.ts
+++ src/types/chrome-remote-interface.d.ts
@@ -1,31 +1,16 @@
 declare module 'chrome-remote-interface' {
   namespace ChromeRemoteInterface {
     interface Client {
-      DOM: {
-        querySelector: (params: any) => Promise<any>;
-        getBoxModel: (params: any) => Promise<any>;
-        childNodeInserted: (callback: (params: any) => void) => void;
-        childNodeRemoved: (callback: (params: any) => void) => void;
-        attributeModified: (callback: (params: any) => void) => void;
-        attributeRemoved: (callback: (params: any) => void) => void;
-        characterDataModified: (callback: (params: any) => void) => void;
-      };
-      Page: {
-        captureScreenshot: () => Promise<{data: string}>;
-        loadEventFired: (callback: () => void) => void;
-      };
-      Runtime: {
-        evaluate: (params: any) => Promise<{result: any}>;
-      };
-      Input: {
-        dispatchMouseEvent: (params: any) => Promise<void>;
-      };
-      Accessibility: {
-        enable: () => Promise<void>;
-        getFullAXTree: () => Promise<{nodes: any[]}>;
-      };
+      DOM: any;
+      Page: any;
+      Runtime: any;
+      Input: any;
+      Accessibility: any;
+      Network: any;
       send: (method: string, params?: any) => Promise<any>;
       close: () => void;
+      on: (event: string, callback: (params: any) => void) => void;
+      once: (event: string, callback: (params: any) => void) => void;
     }
   }
   
