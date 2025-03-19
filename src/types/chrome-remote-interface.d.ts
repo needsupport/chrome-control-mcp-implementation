bb1708b5a38d6be15a16a3fb1687d7ccba3230e2
needsupport <116121446+needsupport@users.noreply.github.com>
2025-03-18 21:00:06 -0700

    Add TypeScript declarations for chrome-remote-interface

--- src/types/chrome-remote-interface.d.ts
+++ src/types/chrome-remote-interface.d.ts
@@ -2,8 +2,8 @@ declare module 'chrome-remote-interface' {
   namespace ChromeRemoteInterface {
     interface Client {
       DOM: {
-        querySelector: (params: any) => Promise<{nodeId: number}>;
-        getBoxModel: (params: any) => Promise<{model: any}>;
+        querySelector: (params: any) => Promise<any>;
+        getBoxModel: (params: any) => Promise<any>;
         childNodeInserted: (callback: (params: any) => void) => void;
         childNodeRemoved: (callback: (params: any) => void) => void;
         attributeModified: (callback: (params: any) => void) => void;
@@ -11,10 +11,8 @@ declare module 'chrome-remote-interface' {
         characterDataModified: (callback: (params: any) => void) => void;
       };
       Page: {
-        navigate: (params: any) => Promise<any>;
+        captureScreenshot: () => Promise<{data: string}>;
         loadEventFired: (callback: () => void) => void;
-        captureScreenshot: (params?: any) => Promise<{data: string}>;
-        enable: () => Promise<void>;
       };
       Runtime: {
         evaluate: (params: any) => Promise<{result: any}>;
@@ -24,20 +22,13 @@ declare module 'chrome-remote-interface' {
       };
       Accessibility: {
         enable: () => Promise<void>;
+        getFullAXTree: () => Promise<{nodes: any[]}>;
       };
       send: (method: string, params?: any) => Promise<any>;
       close: () => void;
     }
-
-    interface Options {
-      target?: any;
-      port?: number;
-      host?: string;
-      protocol?: string;
-      local?: boolean;
-    }
   }
-
-  function ChromeRemoteInterface(options?: ChromeRemoteInterface.Options): Promise<ChromeRemoteInterface.Client>;
-  export = ChromeRemoteInterface;
+  
+  function ChromeRemoteInterface(options?: any): Promise<ChromeRemoteInterface.Client>;
+  export default ChromeRemoteInterface;
 }
